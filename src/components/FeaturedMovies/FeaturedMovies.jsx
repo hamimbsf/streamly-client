e4fca7a95d0dabbd7bfc54dad86129be5486945d
hamimbsf <hamim.bsf@gmail.com>
2024-12-05 02:37:54 +0600

    added some extra features

--- src/components/FeaturedMovies/FeaturedMovies.jsx
+++ src/components/FeaturedMovies/FeaturedMovies.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Movie from "../content/Movie";
+import { Link } from "react-router-dom";
 
 const FeaturedMovies = () => {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:3000/all-movies")
+    fetch("http://localhost:3000/limited-movies")
       .then((res) => res.json())
       .then((data) => {
         // console.log(data);
@@ -13,10 +14,15 @@ const FeaturedMovies = () => {
   }, []);
   return (
     <>
-      <div className="grid grid-cols-3 gap-4 container mx-auto my-16">
+      <div className="grid grid-cols-3 gap-4 container mx-auto mt-16">
         {movies.map((movie) => (
           <Movie key={movie._id} movie={movie} />
         ))}
+      </div>
+      <div className="flex justify-center mt-10 mb-14">
+        <Link to="/all-movies" className="btn text-white w-[30%] bg-red-600">
+          All Movies
+        </Link>
       </div>
     </>
   );
