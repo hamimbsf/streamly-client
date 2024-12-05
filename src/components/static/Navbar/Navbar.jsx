05ec247257ee192b55fd6d2e82a9ef0194f7a0c7
hamimbsf <hamim.bsf@gmail.com>
2024-12-06 05:59:51 +0600

    made navbar more beautiful

--- src/components/static/Navbar/Navbar.jsx
+++ src/components/static/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../../provider/AuthProvider";
+import { RiMenu2Line } from "react-icons/ri";
 
 export const Navbar = () => {
   const { logOut, user } = useContext(AuthContext);
@@ -48,51 +49,48 @@ export const Navbar = () => {
   return (
     <>
       <div className="navbar text-white relative z-[99] px-[5%]">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+        <div className="drawer navbar-start">
+          <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+
+          <div className="drawer-content flex items-center gap-4">
+            {/* Page content here */}
+            <label
+              htmlFor="my-drawer"
+              className="btn btn-circle drawer-button lg:hidden"
             >
+              {/* <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 100 80"
+                fill="currentColor"
+                className="w-8 h-8 hover:scale-110 transition-transform duration-300"
+              >
+                <rect y="10" width="100" height="10" rx="5" />
+                <rect y="35" width="100" height="10" rx="5" />
+                <rect y="60" width="100" height="10" rx="5" />
+              </svg> */}
+              <RiMenu2Line className="text-2xl transition-all hover:scale-110 duration-300 cursor-pointer" />
+            </label>
+            <Link to="/" className="text-3xl text-white font-extrabold">
+              STREAMLY
+            </Link>
+          </div>
+          <div className="drawer-side">
+            <label
+              htmlFor="my-drawer"
+              aria-label="close sidebar"
+              className="drawer-overlay"
+            ></label>
+            <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
               {navItems}
             </ul>
           </div>
-          <Link to="/" className="text-3xl text-white font-extrabold">
-            STREAMLY
-          </Link>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">{navItems}</ul>
         </div>
-        {/* <div className="navbar-end ">
-          <button className="btn btn-circle btn-ghost avatar">
-            <img
-              className="rounded-full w-10"
-              alt="Tailwind CSS Navbar component"
-              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-            />
-          </button>
-        </div> */}
         <div className="navbar-end">
           <details className="dropdown dropdown-end">
             <summary className="btn btn-circle btn-ghost avatar m-1">
-              {/* <button className="btn btn-circle btn-ghost avatar"></button> */}
               {user ? (
                 <img
                   className="rounded-full w-10"
