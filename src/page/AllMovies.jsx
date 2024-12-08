84f18bfe93de5de1fd25549679e0c18af96f100f
hamimbsf <hamim.bsf@gmail.com>
2024-12-08 22:33:44 +0600

    added react helmet to alll movies route

--- src/page/AllMovies.jsx
+++ src/page/AllMovies.jsx
@@ -1,6 +1,7 @@
 import { useLoaderData } from "react-router-dom";
 import Movie from "../components/content/Movie";
 import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
 
 const AllMovies = () => {
   const movies = useLoaderData();
@@ -14,25 +15,30 @@ const AllMovies = () => {
   }, [search]);
 
   return (
-    <div className="container mx-auto my-10 space-y-6">
-      <div className="form-control w-[20%] mx-auto">
-        <input
-          type="text"
-          onChange={(e) => {
-            setSearch(e.target.value);
-          }}
-          placeholder="Search"
-          className="input input-bordered w-24 md:w-auto"
-        />
+    <>
+      <Helmet>
+        <title>Streamly || All Movies</title>
+      </Helmet>
+      <div className="container mx-auto my-10 space-y-6">
+        <div className="form-control w-[20%] mx-auto">
+          <input
+            type="text"
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+            placeholder="Search"
+            className="input input-bordered w-24 md:w-auto"
+          />
+        </div>
+        <div className="grid px-4 md:px-0 md:grid-cols-3 grid-cols-1 gap-4">
+          {allMovies.length === 0 ? (
+            <p> </p>
+          ) : (
+            allMovies.map((movie) => <Movie key={movie._id} movie={movie} />)
+          )}
+        </div>
       </div>
-      <div className="grid px-4 md:px-0 md:grid-cols-3 grid-cols-1 gap-4">
-        {allMovies.length === 0 ? (
-          <p> </p>
-        ) : (
-          allMovies.map((movie) => <Movie key={movie._id} movie={movie} />)
-        )}
-      </div>
-    </div>
+    </>
   );
 };
 
